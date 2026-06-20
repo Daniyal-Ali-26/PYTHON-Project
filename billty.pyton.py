1d833485c6a4ddd29e1f4b482ddc0f298d97bf2a
Daniyal-ali-26 <sayyaddaniyalali@gmail.com>
2026-06-20 02:54:57 -0700

    update billty

--- billty.pyton.py
+++ billty.pyton.py
@@ -1,10 +1,24 @@
 class billty:
+    def __init__(self):
+        self.name=""
+        self.place=""
+        self.LRNum=0.0
+        self.lot=0.0
+        self.price=0.0
+        
     def info(self):
-        self.name = input("Enter the coustemer name :")
-        self.place = input("Enter the place name :")
-        self.LRNum = int(input("Enter the LR Number :"))
-        self.lot = int(input("Enter the LOT :"))
-        self.price = float(input("Enter the Price :"))
+      
+     self.name = input("Enter the coustemer name :")
+     if self.name.isdigit():
+         raise ValueError("Numeric wrods not Accept in Name...")
+         self.place = input("Enter the place name :")
+     if self.place.isdigit():
+            raise ValueError("Numeric wrods not Accept in place...")
+       
+     self.LRNum = int(input("Enter the LR Number :"))
+     self.lot = int(input("Enter the LOT :"))
+     self.price = float(input("Enter the Price :"))
+       
 
     def display(self):
         print()
