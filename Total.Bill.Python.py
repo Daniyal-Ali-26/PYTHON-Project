7eb9a83edff4be3d1b074d46c3b72a772fb49643
Daniyal-ali-26 <sayyaddaniyalali@gmail.com>
2026-06-26 06:24:13 -0700

    try and execpt Update

--- Total.Bill.Python.py
+++ Total.Bill.Python.py
@@ -2,10 +2,28 @@ ch='y'
 total=0
 print("            TOTAL  BILL               ") 
 while ch=='y' or ch=='Y':
-   
-    item = int(input("Enter the ITEM Price :"))
-    total=total+item
-    
-    ch = input("If you want to continue press Y \n for stop press T :")
-    print("___________________________________________________________________")
+        while True:
+            try:
+                item = int(input("Enter the ITEM Price :"))
+                break
+            except ValueError:
+                print("Error, (Invalid Number)  please Re-enter the price!")
+                continue
+            
+        total=total+item
+        while True:
+           try:
+               ch = input("If you want to continue press Y \n for stop press T :")  
+               if ch.isdigit():
+                    raise ValueError("Error, (Invalid Number)  please Re-enter the Option!")
+               
+               break
+           except ValueError as v:
+              print(v) 
+print("___________________________________________________________________")
 print(f"Total Amount=",total)
+
+
+
+
+
